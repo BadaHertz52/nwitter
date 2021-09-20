5c9d7230b07209e33f5509c68d9eb381c889cf5d
BadaHertz52 <lmlwpeace@naver.com>
2021-09-20 15:59:25 +0900

    Add making profile when first log-in
    처음 로그인시 user 가  프로필을 설정하는 기능을 추가함

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,39 +1,48 @@
 import React, { useState, useEffect } from "react";
 
-import { dbService} from "Fbase";
-import Nweet from "components/Nweet";
-import NweetFactory from "components/NweetFactory";
+import { dbService} from "../Fbase";
+import Nweet from "../components/Nweet";
+import NweetFactory from "../components/NweetFactory";
+import myNweets from "../components/GeData";
+import EditProfile from "./EditProfile";
 
 
-const Home =  ({userObj}) => {
+const Home =  ({userObj ,refreshUser}) => {
   const [nweets, setNweets] = useState([]);
-
+  const myProfileStore = dbService.collection("users").doc(userObj.uid) ;
+  
   useEffect(() => {
-      dbService.collection("nweets").onSnapshot((snapshot) => {
+      dbService.collection(`nweets_${userObj.uid}`).onSnapshot((snapshot) => {
       const nweetArray = snapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
       }));
       setNweets(nweetArray);
-    });
-    }, []);
+    })}, []);
 
   return (
     <div>
-      <NweetFactory userObj={userObj}/>
-      <div>
-        { nweets.map((nweet) => (
+      {myProfileStore.creatorId === userObj.uid ?  
+        (
           <>
-          <Nweet
-          key={nweet.id}
-          nweetObj={nweet}
-          userObj ={userObj}
-          isOwner={nweet.creatorId === userObj.uid}
-          />
-
+            <NweetFactory userObj={userObj}/>
+            <div>
+              { nweets.map((nweet) => (
+                <Nweet
+                key={nweet.id}
+                nweetObj={nweet}
+                userObj ={userObj}
+                isOwner={nweet.creatorId === userObj.uid}
+                />
+              ))}
+            </div>
           </>
-        )) }
-      </div>
+        )
+      : 
+        (
+          <EditProfile userObj={userObj} myProfileStore ={myProfileStore} refreshUser={refreshUser}/>
+        )
+      }
     </div>
   );
 };
