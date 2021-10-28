f461f94d213440eeca987b723823d80501c3c4be
BadaHertz52 <lmlwpeace@naver.com>
2021-10-28 16:54:16 +0900

    Make showing following user nweet

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,12 +3,14 @@ import React, { useState, useEffect } from "react";
 import { dbService} from "../Fbase";
 import Nweet from "../components/Nweet";
 import NweetFactory from "../components/NweetFactory";
-import myNweets from "../components/GeData";
+//import myNweets from "../components/GeData";
 import EditProfile from "./EditProfile";
 
 
 const Home =  ({userObj ,refreshUser}) => {
   const [nweets, setNweets] = useState([]);
+  const [followNweets, setFollowNweets] =useState([ ]);
+  const [myNweets, setMyNweets]= useState([]);
   const [IsMyProfile , setIsMyProfile] = useState(false);
   const myProfileStore = dbService.collection("users").doc(userObj.uid) ;
   const findMyProfile =()=> myProfileStore.get()
@@ -22,16 +24,58 @@ const Home =  ({userObj ,refreshUser}) => {
   }).catch((e) => {
     console.log("Error getting document", e)
   });
+
+  const getNweets = ( )=>{
+    const getFollowNweets = async()=>{
+      const currentUserProfile =  dbService.collection('users').doc(userObj.uid);
+      const getFollow = await currentUserProfile.get().then(
+          doc => doc.data().following
+        );
+        console.log("follow" , getFollow);
+      getFollow.forEach(
+        user => {
+          dbService.collection(`nweets_${user}`).onSnapshot((snapshot) => {
+            const nweetArray = snapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }));
+            setFollowNweets(nweetArray);
+          })
+    });
+    };
+    const getMyNweets = async()=>{
+      const nweets = await dbService
+        .collection(`nweets_${userObj.uid}`)
+        .get();
+      const MyNweets = nweets.docs.map(doc => ({
+        id: doc.id ,
+        ...doc.data()}))  ;
+      setMyNweets(MyNweets);
+    };
+    
+    getFollowNweets();
+    getMyNweets();
+    console.log('followNweets' ,followNweets ,'my nweets' ,myNweets)
+    const allNweets = [...followNweets , ...myNweets];
+    allNweets.sort(function(a,b){
+      if(a.createAt < b.createAt){return a} else{return b}
+    });
+
+    setNweets(allNweets);
+    console.log("nweets", nweets)
+    
+  };
+
+
   useEffect(() => {
       findMyProfile();
-      dbService.collection(`nweets_${userObj.uid}`).onSnapshot((snapshot) => {
-      const nweetArray = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setNweets(nweetArray);
-      console.log(IsMyProfile);
-    })}, []);
+      //내가 쓴 글 가져오는 함수 
+      //내가 rt한 글 가져오는 함수
+      //팔로잉 한 사람이 쓴 글  가져오는 함수 - 최근 시간으로 정열해서 가져오는 메시지수 제한 걸기 
+      // 가져온 함수의 nweet을 배열로 만들고 이를 보여주도록
+      getNweets();
+    
+  }, []);
 
   return (
     <div>
