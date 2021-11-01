7c57a8b5cedaa21223767e14b7a879a77996785a
BadaHertz52 <lmlwpeace@naver.com>
2021-11-01 15:26:13 +0900

    Fix calling profile follower

--- src/components/ProfileForm.js
+++ src/components/ProfileForm.js
@@ -2,14 +2,16 @@ import React from 'react';
 import Nweet from './Nweet';
 
 export const ProfileTopForm = ({profile , follower} )=>{
-  
+  console.log(profile.following);
   return(
     <section>
       <div>
         <img src={profile.photoUrl} width="150px" height="100px"  alt="profile"/>
         <span>{profile.userName}</span>
       </div>
-      <div>following : {profile.following[0] === undefined ? 0 :profile.following.length }</div>
+      {profile.following && (
+        <div>following : {profile.following[0] === undefined ? 0 :profile.following.length }</div>
+      )}
       <div> follower: {follower[0] === undefined ? 0 :follower.length  }</div>
     </section>
   )
