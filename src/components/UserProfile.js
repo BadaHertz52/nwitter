e1dd761045d92332653c69c3523831d305543d93
BadaHertz52 <lmlwpeace@naver.com>
2021-11-08 00:03:56 +0900

    Modify object name and  parameter

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,31 +1,38 @@
 
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { getProfile } from './GetData';
 
 
 const UserProfile = ({nweetObj }) => {
   const [userProfile, setUserProfile] =useState({});
-  const getUserProfile = () => getProfile(nweetObj.creatorId , setUserProfile); 
-
-    useEffect(()=>{
+  const getUserProfile = async() =>{
+    await getProfile(nweetObj.creatorId , setUserProfile); 
+    }
+  useEffect(()=>{
     getUserProfile();
-
   },[]);
-  const creatorId =JSON.stringify( userProfile.creatorId);
+
+  const id =JSON.stringify( nweetObj.creatorId);
   return (
     <>
+    {userProfile !== {} &&
+    (
       <div >
-        <Link id={creatorId} to={{
-          pathname:"/profile",
-          state :{
-            userProfile :userProfile
-          }
-        }}>
-          <img src={userProfile.photoUrl}  
-            width="50px" height="50px"    alt="profile"/>
-          <span>{userProfile.userName}</span>
-        </Link>
-      </div>
+      <Link id={id} to={{
+        pathname:"/profile",
+        state :{
+          userProfile :userProfile
+        }
+      }}>
+        <img src={userProfile.photoUrl}  
+          width="50px" height="50px"    alt="profile"/>
+        <span>{userProfile.userName}</span>
+      </Link>
+    </div>
+    )
+    }
+
 
     </>
   )
