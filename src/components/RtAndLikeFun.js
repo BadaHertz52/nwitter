3c2b48023cc914456f9e89a9812eb902d62ff867
BadaHertz52 <lmlwpeace@naver.com>
2021-11-13 21:45:28 +0900

    Refaoctor rt and heart alarm storage  by refactoring alarm function
    - 알람 저장소를 프로파일로 변경함에 따른 코드 수정과 오타 수정

--- src/components/RtAndLikeFun.js
+++ src/components/RtAndLikeFun.js
@@ -6,54 +6,98 @@ import { getProfile, getProfileDoc } from "./GetData";
 
 const RtAndLikeFun = ( {nweetObj ,userObj})=> {
   const date = Date.now();
-  const [profile, setProfile] = useState({});
+  const [ownerProfile, setOwnerProfile] = useState({alarm:[]});
+  const [whoProfile, setWhoProfile] = useState({alarm:[]});
+  const [rtValue ,setRtValue] =useState(false);
+  const [heartValue ,setHeartValue] =useState(false);
+  const newAlarm =(what)=>({userId:userObj.uid , creatorId : nweetObj.creatorId, createdAt: nweetObj.createdAt, value: what });
+  const rtBtn = document.getElementsByClassName("rtBtn");
+  const heartBtn = document.getElementsByClassName("heartBtn");
+
+  const checkAlarm = ()=>{
+    ownerProfile.alarm[newAlarm("rt")] && setRtValue(true);
+    rtValue === true && (rtBtn.style.backgroundColor = "lightblue" );
+    ownerProfile.alarm[newAlarm("heart")] && setHeartValue(true);
+    heartValue === true && (heartBtn.style.backgroundColor = "lightblue" );
+  };
 
   useEffect(()=>{
-  getProfile(nweetObj.creatorId ,setProfile);
-  },[])
-  const updateAlram = (data)=> {
-    const userAlram  = profile.alram ; 
-    userAlram.push(`${userObj.displayName}님이 내 트윗을 ${data}.`);
-    //추후에  displayName 변경 시에 따른 알람 내용 변경을 코딩해야함 
-    profile.userName !== userObj.displayName && (
-      getProfileDoc(nweetObj.creatorId).set({
-      alram :userAlram 
-    }, {merge:true}))
+  getProfile(nweetObj.creatorId ,setOwnerProfile);
+  nweetObj.who && getProfile(nweetObj.who ,setWhoProfile) ;
+  checkAlarm();
+  },[]);
+
+  const updateAlram = (what)=> {
+    //data : nweetObj 
+    const ownerAlarm  = ownerProfile.alarm ; 
     
+    //  nweet  원래 작성자
+    ownerAlarm.unshift(newAlarm(what));
+    
+    getProfileDoc(nweetObj.creatorId).set({alarm: ownerAlarm},{merge:true}) ;
+    // rt, heart한 작성자 
+    if(nweetObj.who){
+      const whoAlarm = whoProfile.alarm ; 
+      whoAlarm.unshift(newAlarm(what));
+    getProfileDoc(nweetObj.who).set({alarm: whoAlarm},{merge:true}) ;
+    } 
   };
+
+  // const deleteAlarm = () => {
+  //   const ownerAlarm = ownerProfile.alarm.fi
+  //   getProfileDoc(nweetObj.creatorId).
+  // }
   const Rt =(event)=>{ 
     event.preventDefault();
-    const rt ={
-      text: nweetObj.text,
-      value :"rt",
-      createAt: date,
-      who:userObj.uid , 
-      creatorId: nweetObj.creatorId,
-      attachmentUrl : nweetObj.attachmentUrl
-    };
-    dbService.collection(`nweets_${userObj.uid}`).doc(`${date}`).set(rt);
-    updateAlram('리트윗 했습니다.');
+    console.log(rtValue );
+    
+    if(rtValue === false){
+      const rt ={
+        text: nweetObj.text,
+        value :"rt",
+        createdAt: nweetObj.createdAt,
+        who:userObj.uid , 
+        creatorId: nweetObj.creatorId,
+        attachmentUrl : nweetObj.attachmentUrl
+      };
+      updateAlram("rt");
+      setRtValue(true);
+      dbService.collection(`nweets_${userObj.uid}`).doc(`${date}`).set(rt);
+    }else {
+      dbService.collection(`nweets_${userObj.uid}`).where("text" , "===" , `${nweetObj.text}`).delete();
+      //deltealarm (); 
+      setRtValue(false);
+    }
+    checkAlarm();
     };
 
-  const sendHeart = () => {
-    const heart ={
-      text: nweetObj.text,
-      value :"heart",
-      createAt:date,
-      creatorId: nweetObj.creatorId,
-      who:userObj.uid ,
-      attachmentUrl : nweetObj.attachmentUrl
-    };
-    dbService.collection(`nweets_${userObj.uid}`).doc(`${date}`).set(heart);
-    updateAlram('마음에 들어합니다.');
+  const sendHeart = (event) => {
+    event.preventDefault();
+    if(heartValue === false){
+      const heart ={
+        text: nweetObj.text,
+        value :"heart",
+        createdAt:nweetObj.createdAt,
+        creatorId: nweetObj.creatorId,
+        who:userObj.uid ,
+        attachmentUrl : nweetObj.attachmentUrl
+      };
+      dbService.collection(`nweets_${userObj.uid}`).doc(`${date}`).set(heart);
+      updateAlram( "heart");
+      setHeartValue(true);
+    }else {
+      dbService.collection(`nweets_${userObj.uid}`).where("text" , "===" , `${nweetObj.text}`).delete();
+      setHeartValue(false);
+    }
+    checkAlarm();
   };
 
   return(
     <>
-      <button onClick={Rt}>
+      <button className="rtBtn" onClick={Rt} value={rtValue}>
         <AiOutlineRetweet/>
       </button>
-      <button onClick={sendHeart}>
+      <button className="heartBtn" onClick={sendHeart} value={heartValue}>
         <AiOutlineHeart/>
       </button>
     </>
