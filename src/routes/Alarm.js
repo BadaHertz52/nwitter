405ba91b3bcc9d55b17dc0eac57e70b5ed5841d5
BadaHertz52 <lmlwpeace@naver.com>
2021-11-13 17:18:37 +0900

    Fix error getting promise
    -  서버의 정보를 promise 형태로 가져오는 것을 고침
    - 보충해야 할 것: rt, heart 에 따라 유저와 nweet를 가져오는 것을 분리하고 이를 html에 나타내는 것

--- src/routes/Alarm.js
+++ src/routes/Alarm.js
@@ -1,52 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { getProfile, getProfileDoc } from '../components/GetData';
+import {  getProfileDoc } from '../components/GetData';
+import { dbService } from '../Fbase';
 
-const Alarm = ({userObj}) => {
-  const [profile, setProfile] =useState({
-    alarm:[]
-  }) ;
-  const [alarms ,setAlarms] =useState([]);
-  const makeAlarm =async() =>{
-    // userName 가져오기 
-    const nameArry = await Promise.all(
-      profile.alarm.map( a =>{
-      return getProfileDoc(a.who).get().then(doc=>doc.data().userName)
-    }));
-    // userName 과  rt or heart  조합하기
-    for(let i=0 ;i<profile.alarm.length ;i++){
-      const newAlarm ={who:nameArry[i], how: profile.alarm[i].how};
-      alarms[newAlarm] !== undefined && alarms.push(newAlarm);
-      setAlarms(alarms);
-    };
-      console.log(alarms ,alarms[0] !== undefined)
+const Alarm =async ({userObj}) => {
+  const getDocs = await dbService.collection(`nweets_${userObj.uid}`).where('alarm' , '==' , true).get();
+  const Docs = getDocs.docs.map(doc=> doc.data());
+  const getUserName = (id)=>  getProfileDoc(id).get().then(doc=> ({
+    name: doc.data().userName ,
+    userId: id}));
+
+  const getAlarm = async () =>{
+    await Promise.all 
+      (Docs.map( (doc) =>{
+        const getNames =(alarm)=> Promise.all ( alarm.map(id => getUserName(id) ).concat({nweet:doc})) ;
+        const heartNames= getNames(doc.heartAlarm);
+        return ( 
+        heartNames
+      )
+    }))
+    .then(values => console.log(values ))
+    .catch(err => console.log('Erro', err));
   };
 
   useEffect(()=>{
-    getProfile(userObj.uid, setProfile);
-    makeAlarm();
-  } ,[]);
+    //getRtAlarm();
+    getAlarm();
+  },[])
   return (
     <>
-      { alarms[0]!==undefined &&
-      (alarms.map( (a) => (
-      <div>
-        <Link 
-        to={{
-        pathname:"/nweet",
-        state :{
-          nweetObj : a.what,
-          userObj : userObj,
-          isOwner : true
-        }}}>
-          <div>
-          {a.who} 님이 {a.how === "rt" ? '리트윗' : '마음에 들어함'} 함
-          </div>
-        </Link>
-      </div>)))
-      }
-      <div>...</div>
-      
+      <div id='alarm'>
+
+      </div>
     </> 
 )}
 export default Alarm ;
