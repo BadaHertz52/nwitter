1604209de3285e4e5540b3e5d55ce57ed34e8e15
BadaHertz52 <lmlwpeace@naver.com>
2021-11-17 18:47:05 +0900

    Make function  showing nweet about alarm when user click alarm

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -5,10 +5,11 @@ import UserProfile from './UserProfile';
 import { getProfile } from './GetData';
 import { AiOutlineRetweet ,AiOutlineHeart } from "react-icons/ai";
 import { useHistory } from 'react-router';
-//edit, delete 
+//edit, delete
 const Nweet =({nweetObj , userObj ,isOwner  }) =>{
   const historyState =useHistory().location.state ;
-  if( nweetObj === undefined && userObj ===undefined && isOwner ===undefined){
+  if( nweetObj === undefined){
+    console.log(historyState);
     nweetObj = historyState.nweetObj;
     userObj =historyState.userObj ;
     isOwner =historyState.isOwner;
@@ -20,7 +21,7 @@ const Nweet =({nweetObj , userObj ,isOwner  }) =>{
     if(ok){
       //delete nweet
       await dbService.doc(`nweets_${userObj.uid}/${nweetObj.id}`).delete();
-      //delete photo 
+      //delete photo
       await storageService.refFromURL(nweetObj.attachmentUrl).delete();
     }
   };
@@ -28,35 +29,40 @@ const Nweet =({nweetObj , userObj ,isOwner  }) =>{
   useEffect(()=>{
     nweetObj.who && getProfile(nweetObj.who , setUserProfile)
     },[]);
-  
+
   return(
-    <div className="nweet" >
-      {nweetObj.value === "rt" && 
-        <div> 
-          <AiOutlineRetweet/> {userProfile.creatorId === userObj.uid  ? '내가' : `${userProfile.userName}님이`} 리트윗함
-        </div>
+    <>
+      {nweetObj !==undefined &&
+            <div className="nweet" >
+            {nweetObj.value === "rt" &&
+              <div>
+                <AiOutlineRetweet/> {userProfile.creatorId === userObj.uid  ? '내가' : `${userProfile.userName}님이`} 리트윗함
+              </div>
+            }
+            {nweetObj.value === "heart" &&
+              <div>
+                <AiOutlineHeart/> {userProfile.creatorId === userObj.uid  ? '내가' : `${userProfile.userName}님이`} 이 트윗을 마음에 들어함
+              </div>
+            }
+            <UserProfile nweetObj ={nweetObj}/>
+            <h4>{nweetObj.text}</h4>
+            <div>
+              { nweetObj.attachmentUrl &&
+              <img src={nweetObj.attachmentUrl}  max-width="300px" height="150px" alt="Nweet_photofile"/>}
+            </div>
+            <div className="rtAndLikeFun">
+              <RtAndLikeFun nweetObj={nweetObj} userObj={userObj}/>
+            </div>
+
+            {isOwner &&
+              <>
+                <button onClick={onDeleteClick}>Delete Nweet</button>
+              </>
+            }
+          </div>
       }
-      {nweetObj.value === "heart" &&
-        <div>
-          <AiOutlineHeart/> {userProfile.creatorId === userObj.uid  ? '내가' : `${userProfile.userName}님이`} 이 트윗을 마음에 들어함
-        </div>
-      }
-      <UserProfile nweetObj ={nweetObj}/>
-      <h4>{nweetObj.text}</h4>
-      <div>
-        { nweetObj.attachmentUrl && 
-        <img src={nweetObj.attachmentUrl}  max-width="300px" height="150px" alt="Nweet_photofile"/>}
-      </div>
-      <div className="rtAndLikeFun">
-        <RtAndLikeFun nweetObj={nweetObj} userObj={userObj}/>
-      </div>
-      
-      {isOwner && 
-        <>
-          <button onClick={onDeleteClick}>Delete Nweet</button>
-        </>
-      }
-    </div>
+    </>
+
   )
 }
 
