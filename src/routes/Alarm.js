14ae512244432d0548dc619d4e39af9bf5506dce
BadaHertz52 <lmlwpeace@naver.com>
2021-11-17 18:47:24 +0900

    Arrange code

--- src/routes/Alarm.js
+++ src/routes/Alarm.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { AiFillInstagram } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
-import { getNweets, getProfile, getProfileDoc } from '../components/GetData';
+import { getProfile, getProfileDoc } from '../components/GetData';
 import { dbService } from '../Fbase';
 
 const Alarm = ({userObj}) => {
@@ -13,12 +12,20 @@ const Alarm = ({userObj}) => {
   useEffect(()=>{
     getProfile(userObj.uid, setProfile);
     async function makeAlarm(){
-      const alarm = await Promise.all (profile.alarm.map( async (a) =>{
-        const user_name = await getProfileDoc(a.userId).get().then(doc=>doc.data().userName);
-        const nweetData = await dbService.collection(`nweets_${a.creatorId}`).doc(`${a.createdAt}`).get().then(doc => doc.data());
-        return {userName:user_name , nweet:nweetData , value: a.value}
-      }));
-    setAlarms(alarm);
+      const alarm = await Promise.all(
+        profile.alarm.map( async (a) =>{
+          const user_name = await getProfileDoc(a.userId).get().then(doc=>doc.data().userName);
+          const nweetData = await dbService.collection(`nweets_${a.creatorId}`).doc(`${a.createdAt}`)
+                                  .get()
+                                  .then(doc => doc.data());
+          return {
+            userName:user_name , 
+            nweet:nweetData , 
+            value: a.value
+          }
+        })
+      );
+      setAlarms(alarm);
     }
     makeAlarm();
   } ,[profile.alarm])
@@ -32,10 +39,11 @@ const Alarm = ({userObj}) => {
         to={{
         pathname:"/nweet",
         state :{
-          nweetObj : a.nweet,
+          nweetObj :a.nweet,
           userObj : userObj,
           isOwner : true
-        }}}>
+        }
+        }}>
           <div>
           {a.userName} 님이 내 트윗을  {a.value === "rt" ? '리트윗' : '마음에 들어함'} 함
           </div>
