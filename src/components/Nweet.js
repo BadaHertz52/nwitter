3df6ef738a0bea94809b58aca28052a909296855
BadaHertz52 <lmlwpeace@naver.com>
2021-11-23 15:00:01 +0900

    Add codes to show the original text of the reply.

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,58 +1,115 @@
 import { dbService, storageService } from '../Fbase';
 import React, { useEffect, useState } from 'react';
-import RtAndLikeFun from './RtAndLikeFun';
+import RnAndLikeFun from './RnAndLikeFun';
 import UserProfile from './UserProfile';
 import { getProfile } from './GetData';
 import { AiOutlineRetweet ,AiOutlineHeart } from "react-icons/ai";
 import { useHistory } from 'react-router';
+import '../css/nweet.css' ;
+import {FiMessageCircle} from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 //edit, delete
-const Nweet =({nweetObj , userObj ,isOwner  }) =>{
+const Nweet =({nweetObj , userobj ,isOwner  }) =>{
   const historyState =useHistory().location.state ;
   if( nweetObj === undefined){
     nweetObj = historyState.nweetObj;
-    userObj =historyState.userObj ;
+    userobj =historyState.userobj ;
     isOwner =historyState.isOwner;
   }
-  const [userProfile, setUserProfile] =useState({});
-
+  const [whoProfile, setWhoProfile] =useState();
+  const [ownerProfile, setOwnerProfile] =useState();
+  const [nweetClassName ,setNCName]=useState("nweet");
+  const [nweetContentClassName ,setNCCName]=useState("nweet_content");
   const onDeleteClick = async () =>{
     const ok = window.confirm("Are you sure you want to delete this nweet?");
     if(ok){
       //delete nweet
-      await dbService.doc(`nweets_${userObj.uid}/${nweetObj.id}`).delete();
+      await dbService.doc(`nweets_${userobj.uid}/${nweetObj.id}`).delete();
       //delete photo
       await storageService.refFromURL(nweetObj.attachmentUrl).delete();
     }
   };
-
+  const chagneClassName =()=>{
+  if(nweetObj.value === "answer"){
+    setNCName("nweet answer");
+    setNCCName("nweet_content answer");
+  }
+};
   useEffect(()=>{
-    nweetObj.who && getProfile(nweetObj.who , setUserProfile)
+    const getUsersProfile = async() =>{
+      await getProfile(nweetObj.creatorId , setOwnerProfile); 
+      nweetObj.who && await getProfile(nweetObj.who , setWhoProfile)
+      };
+    getUsersProfile();
+    chagneClassName();
     },[]);
 
   return(
     <>
-      {nweetObj !==undefined &&
-            <div className="nweet" >
+      {nweetObj !==undefined && 
+        <div className={nweetClassName} >
+          <div className="nweet_value">
             {nweetObj.value === "rt" &&
               <div>
-                <AiOutlineRetweet/> {userProfile.creatorId === userObj.uid  ? '내가' : `${userProfile.userName}님이`} 리트윗함
+                <AiOutlineRetweet/> 
+                {whoProfile.creatorId === userobj.uid  ? 
+                '내가' 
+                : 
+                `${whoProfile.userName}님이`} 리트윗함
               </div>
             }
             {nweetObj.value === "heart" &&
               <div>
-                <AiOutlineHeart/> {userProfile.creatorId === userObj.uid  ? '내가' : `${userProfile.userName}님이`} 이 트윗을 마음에 들어함
+                <AiOutlineHeart/> 
+                {whoProfile.creatorId === userobj.uid  ? 
+                '내가' 
+                : 
+                `${whoProfile.userName}님이`} 이 트윗을 마음에 들어함
               </div>
             }
-            <UserProfile nweetObj ={nweetObj}/>
-            <h4>{nweetObj.text}</h4>
-            <div>
+            {nweetObj.value === "answer" &&
+              <Nweet nweetObj={nweetObj.answer} isOwner={false} userobj={userobj} />
+            }
+          </div>
+          <div className={nweetContentClassName}>
+            <UserProfile nweetObj ={nweetObj}/> 
+            <div className="nweet_content_text" >{nweetObj.text}</div>
+            <div  className="nweet_content_attachment">
               { nweetObj.attachmentUrl &&
               <img src={nweetObj.attachmentUrl}  max-width="300px" height="150px" alt="Nweet_photofile"/>}
             </div>
-            <div className="rtAndLikeFun">
-              <RtAndLikeFun nweetObj={nweetObj} userObj={userObj}/>
+            {nweetObj.citingNweet && nweetObj.citingNweet !== {} &&(
+              <div className="nweet_cn">
+                <UserProfile nweetObj ={nweetObj.citingNweet}/> 
+                <div className="nweet_content_text" >{nweetObj.citingNweet.text}</div>
+                <div  className="nweet_content_attachment">
+                  { nweetObj.citingNweet.attachmentUrl &&
+                    <img src={nweetObj.citingNweet.attachmentUrl}  max-width="300px" height="150px" alt="Nweet_photofile"/>}
+                </div>
+              </div>
+            )}
+          </div>
+          <div className="nweet_fun">
+            <div className="nweet_fun_answer">
+              <Link to={{
+                pathname:"/nweet",
+                state :{
+                  value:"answer",
+                  nweetObj :nweetObj,
+                  userobj:userobj
+                }
+              }}>
+                <FiMessageCircle/>
+              </Link>
+            </div>
+            <div className="nweet_fun_rtAndLikeFun">
+              <RnAndLikeFun nweetObj={nweetObj} userobj={userobj} whoProfile={whoProfile
+              } ownerProfile={ownerProfile}/>
             </div>
 
+          </div>
+            
+            
             {isOwner &&
               <>
                 <button onClick={onDeleteClick}>Delete Nweet</button>
