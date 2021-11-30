cf5155d1fc07a5e58657c319861beb2580b8b670
BadaHertz52 <lmlwpeace@naver.com>
2021-11-30 16:48:04 +0900

    Fix error not find citingNweet
    - rt 알림시 rn에 citingNweet, alarm을 설정하지 않아서 Nweet..js와 충돌하는 오류가 있었고 이를 수정함

--- src/components/RnAndLikeFun.js
+++ src/components/RnAndLikeFun.js
@@ -6,24 +6,22 @@ import { dbService } from "../Fbase";
 import {  getProfileDoc } from "./GetData";
 
 const RnAndLikeFun = ( {nweetObj ,userobj , ownerProfile ,whoProfile,})=> {
-  const initialRn ={
+
+  const [heart,setHeart]=useState({
+    empty :undefined ,
+    id : "" //heart 했을 시 해당 doc.id
+  });
+  const [rn,setRn]=useState({
     empty : undefined ,
     id : ""
-  };
-  const initialHeart ={
-    empty :undefined ,
-    id : ""
-  };
-  const [heart,setHeart]=useState(
-    initialHeart);
-  const [rn,setRn]=useState( initialRn
+  }
   );
   const [rnBtn,setRnBtn]= useState("rhBtn"); //rn & heart btn
   const [rnPopup, setRnPop] =useState("rnPopup");
   const [heartBtn,setHeartBtn]= useState("rhBtn");
-  const date = Date.now();
+  const date =JSON.stringify(Date.now()) ;
   const body= document.querySelector("body");
-  const newAlarm =(what)=>({userId:userobj.uid , creatorId : nweetObj.creatorId, createdAt: nweetObj.createdAt, value: what });
+
   const userobjCollection = dbService.collection(`nweets_${userobj.uid}`)  ;
   const whereRn = userobjCollection
   .where("creatorId" , "==" , `${nweetObj.creatorId}` )
@@ -57,11 +55,11 @@ const RnAndLikeFun = ( {nweetObj ,userobj , ownerProfile ,whoProfile,})=> {
     rn.empty ? setRnBtn("rhBtn") : setRnBtn("rhBtn on");
     heart.empty ? setHeartBtn("rhBtn") : setHeartBtn("rhBtn on");
   };
+  const newAlarm =(what)=>({userId:userobj.uid , creatorId : nweetObj.creatorId, createdAt: nweetObj.createdAt, value: what });
 
   const updateAlram = (what)=> {
     //data : nweetObj
     const ownerAlarm  = ownerProfile.alarm ;
-
     //  nweet  원래 작성자
     ownerAlarm.unshift(newAlarm(what));
     getProfileDoc(nweetObj.creatorId).set({alarm: ownerAlarm},{merge:true}) ;
@@ -104,7 +102,6 @@ const RnAndLikeFun = ( {nweetObj ,userobj , ownerProfile ,whoProfile,})=> {
   };
   const popupRn =async(event)=>{
     event.preventDefault();
-    console.log(rn.empty)
     if( rn.empty === false) {
       //rn 해제 
       console.log("CANCLE RN" )
@@ -115,35 +112,36 @@ const RnAndLikeFun = ( {nweetObj ,userobj , ownerProfile ,whoProfile,})=> {
       if(rn.empty === true){        // rn와 인용 
           //popup  창 띄우기 
         setRnPop("rnPopup show");
-        console.log(rnPopup);
           //popup 창 닫기
         body.addEventListener('click', popup);
       }
   };
   const reNweet =(event)=>{
-    (async()=> await checkAlarm())();
-    console.log("rn버튼 누름" ,"rn 했던 글 인지 여부" ,!rn.empty)
     event.preventDefault();
     console.log("re nweet");
     const rn ={
       text: nweetObj.text,
       value :"rn",
       createdAt: nweetObj.createdAt,
-      who:userobj.uid ,
+      who:userobj.uid , //who는 rt,heart 시에 생성됨 
       creatorId: nweetObj.creatorId,
-      attachmentUrl : nweetObj.attachmentUrl
+      attachmentUrl : nweetObj.attachmentUrl,
+      alarm:false,
+      rnAlarm:[],
+      heartAlarm:[],
+      answer: null,
+      citingNweet:null,
     };
     userobj.uid !== nweetObj.creatorId && updateAlram("rn");
     userobjCollection.doc(`${date}`).set(rn);
-    setRn({empty:false, id:""});
+    setRn({empty:false, id:date}); 
     };
 
   const sendHeart = async (event) => {
-    console.log("heart버튼 누름" , "heart 한 적 있는 글인지 " ,!heart.empty)
+    console.log("heart버튼 누름")
     event.preventDefault();
-    await checkAlarm();
     if(heart.empty === true){
-      console.log("heart");
+      console.log("send heart");
       const heart ={
         text: nweetObj.text,
         value :"heart",
@@ -156,13 +154,13 @@ const RnAndLikeFun = ( {nweetObj ,userobj , ownerProfile ,whoProfile,})=> {
       userobj.uid !== nweetObj.creatorId && updateAlram( "heart");
       setHeart({
         empty:false ,
-        id: heart.id
+        id:date
       });
     }else if( heart.empty === false){
-      console.log("delete")
-      await userobjCollection.where("text" , "==" , `${nweetObj.text}`).delete();
-    userobj.uid !== nweetObj.creatorId && deleteAlarm("heart");
-    setHeart({
+      console.log("delete heart")
+      await userobjCollection.doc(`${heart.id}`).delete();
+      userobj.uid !== nweetObj.creatorId && deleteAlarm("heart");
+      setHeart({
       empty:true ,
       id:""
     });
@@ -174,8 +172,12 @@ const RnAndLikeFun = ( {nweetObj ,userobj , ownerProfile ,whoProfile,})=> {
   useEffect(()=>{
     checkAlarm();
     return()=>{
-      setRn(initialRn);
-      setHeart(initialHeart);
+      setRn({
+        empty :undefined ,
+      });
+      setHeart({
+        empty :undefined ,
+      });
     }
   },[]);
 
