5dd0f263cf493f93d5d47b782678c17cd7aa804b
BadaHertz52 <lmlwpeace@naver.com>
2022-01-29 19:11:57 +0900

    Make Loading component and style of Loading

---
+++ src/components/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../asset/loading.css'
+
+const Loading =()=>{
+  return (
+    <div class='container'>
+      <div class="loader">
+        <span></span>
+      </div>
+    </div>
+  )
+};
+
+export  default Loading;
