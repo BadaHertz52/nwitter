1ab42f89a102721cb0c2afcd0c03e4ed7446e583
BadaHertz52 <lmlwpeace@naver.com>
2022-01-30 17:08:23 +0900

    Remove Loading from HomeNweets

--- src/components/HomeNweets.js
+++ src/components/HomeNweets.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { useContext } from "react/cjs/react.development";
 import { NweetContext } from "../context/NweetContex";
-import Loading from "./Loading";
 import Nweet from "./Nweet";
 
 
 const HomeNeets =({userobj})=> {
   const {allNweets}=useContext(NweetContext);
+
   return (
       <section className="nweets">
       <div className='nweets_nweet'>
-        { (allNweets!== undefined) ?
+        { (allNweets!== undefined) &&
         allNweets.map((nweet) => (
           <Nweet
           key={`nweet'_${nweet.docId}`}
@@ -20,8 +20,6 @@ const HomeNeets =({userobj})=> {
           answer={false}
           />
         ))
-        :
-        <Loading/>
         }
       </div>
     </section>
