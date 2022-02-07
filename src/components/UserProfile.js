fb4aafeb54edb0ab0aee0c1754abb83a29cdf9f1
BadaHertz52 <lmlwpeace@naver.com>
2022-02-07 18:38:02 +0900

    DOC) Move userProfileDispatch to Profile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import {  useNavigate ,useLocation} from 'react-router-dom';
-import { useContext } from 'react/cjs/react.development';
-import { UserContext } from '../context/UserContext';
-import { dbService } from "../Fbase";
+
 const UserProfile = ({profile}) => {
   const navigate=useNavigate();
   const location=useLocation()
-  const {userDispatch}=useContext(UserContext);
+
   const goProfile =async()=>{
-    const getDocs = await  dbService
-    .collection(`nweets_${profile.uid}`).get();
-    const nweets =getDocs
-    .docs.map(doc =>({ id:doc.id ,...doc.data()}));    
-    
-    userDispatch({
-      type:"GET_USER_DATA",
-      userProfile :profile,
-      userNweets:nweets
-    });
     navigate(`/${profile.userId}` ,{state:{
       previous:location.pathname,
+      userProfile:profile,
       userId:profile.userId ,
-      value:"userProfile"}})
+      value:"userProfile",
+    }})
   }
   return (
     <>
