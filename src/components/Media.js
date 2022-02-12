c10d2297abee9b7009e4647e4631cdb3bbff9ae5
BadaHertz52 <lmlwpeace@naver.com>
2022-02-12 23:29:22 +0900

    DOC) Correct typo error FEAT) Modify medias form adding media and id to fix key error which is key values overlap

--- src/components/Media.js
+++ src/components/Media.js
@@ -8,13 +8,13 @@ const Media =({who})=>{
   const {userTweets} =useContext(UserContext);
   const [medias, setMedias] =useState([]);
   const getMedias =async(tweets)=>{
-    const filtedtweets =tweets.filter(tweet=> tweet.attachmentUrl !=="");
-    let array = filtedtweets.map(tweet => tweet.attachmentUrl);
+    const filteredtweets =tweets.filter(tweet=> tweet.attachmentUrl !=="");
+    let array = filteredtweets.map(tweet => ({media:tweet.attachmentUrl, id: filteredtweets.indexOf(tweet)}));
     if(array.length !==6){
       array.length >6 ? array =array.slice(0,6): addGrey();
       function addGrey (){
         for (let i = 0; array.length<6; i++) {
-          array.push(grey);
+          array.push({media:grey , id:array.length+1});
         };
       }
     };
@@ -35,9 +35,9 @@ const Media =({who})=>{
   },[myTweets ,userTweets]);
 
     return (
-      <div id="media">
-        { medias.map( media =>  
-        <img  src={media} alt="side_media" key={`media`}/>
+      <div id="medias">
+        { medias.map( media => 
+          <img  className='media' src={media.media} alt="side_media" key={media.id}/>
         )}
       </div>
     )
