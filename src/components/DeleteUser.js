7f265df2be0b33187b9bd1eb0eeba42f6598d9bd
BadaHertz52 <lmlwpeace@naver.com>
2022-02-19 18:17:29 +0900

    FEAT) Change DeleteUser component which return html code and Make cleanUPFollow function and conditions for when noticiations in myProfile hasn't notification or myTweet is not existence   and laoding object

--- src/components/DeleteUser.js
+++ src/components/DeleteUser.js
@@ -1,110 +1,185 @@
-import React  from 'react';
+import React, { useContext, useState }  from 'react';
+import { BsTwitter } from 'react-icons/bs';
+import { useNavigate } from 'react-router';
+import { ProfileContext } from '../context/ProfileContex';
+import { TweetContext } from '../context/TweetContex';
 import authSerVice, { dbService, storageService} from '../Fbase';
 import { getTweetDoc, getTweetsDocs, getProfileDoc } from './GetData';
+import Loading from './Loading';
 
+const DeleteUser =({userobj} )=> {
+    const {myProfile}=useContext(ProfileContext);
+    const {myTweets}=useContext(TweetContext);
 
-const DeleteUser =async(myProfile, myTweets, setDeleteError  )=> {
-    const currentUser =authSerVice.currentUser;
-
+    const navigate =useNavigate();
+    const [deleteError,setDeleteError] =useState(false);
+    const [loading, setLoading]=useState(false);
     //clean up following ,follower 
-    if(myProfile.following[0]!==undefined){
-      myProfile.following.forEach(async(user)=>
-      {
-        await getProfileDoc(user).get().then(result=>{
-          const profile =result.data();
-          const newNotifications =profile.notifications.filter(n=> (n.value!=="following"|| n.user !== currentUser.uid));
-          const newFollower = profile.follower.filter(f=> f!== currentUser.uid);
-
-          getProfileDoc(user).set({
-            notifications:newNotifications
-          },{merge:true});
-
-          getProfileDoc(user).set({
-            follower:newFollower
-          },{merge:true})
-          
-        });
-
-        
-      })
-    };
-
-    if(myProfile.follower[0]!== undefined){
-      myProfile.follower.forEach(async(user)=>{
-        await getProfileDoc(user).get().then(
-          (result) => {
-            const profile= result.data();
-            const newFollowing =profile.following.filter(f=> f!==currentUser.uid);
+    const cleanUpFollow =async()=>{
+      if(myProfile.following[0]!==undefined){
+        myProfile.following.forEach(async(user)=>
+        {
+          await getProfileDoc(user).get().then(result=>{
+            const profile =result.data();
+            const newNotifications =profile.notifications.filter(n=> (n.value!=="following"|| n.user !== userobj.uid));
+            const newFollower = profile.follower.filter(f=> f!== userobj.uid);
+  
             getProfileDoc(user).set({
-              following: newFollowing
+              notifications:newNotifications
+            },{merge:true});
+  
+            getProfileDoc(user).set({
+              follower:newFollower
             },{merge:true})
-          }
-        );
-      })
-    };
-    
-    //clean up rt,heart,answer,qt
-      // 유저가 작성한 tweet에 대한 rt.heart,qt 삭제
-    const targets =myProfile.notifications.filter(n=> n.docId !== null);
-
-    if(targets[0]!== undefined){
-      targets.forEach(async(target)=>{
-        getTweetsDocs(target.user, target.aboutDocId).then(
-          result=> {
-            const docs= result.docs;
-            docs.forEach(doc=> doc.delete())
-          }
-        );
-      })
-    }
-    // 유저가 rt.heart,qt,answer 한 것 에 대한 삭제
-    const targetTweets= myTweets.filter(tweet => tweet.about !== null);
-    
-    if(targetTweets[0]!== undefined){
-      targetTweets.forEach(async(tweet)=>{
-        const targetUser = tweet.about.creatorId;
-        await getTweetDoc(targetUser, tweet.about.docId).then(doc=> {
-          const userTweet= doc.data();
-          const newNotifications= userTweet.notifications.filter(n=>n.user!== currentUser.uid);
-          dbService.collection(`tweets_${targetUser}`).doc(`${tweet.about.docId}`).set({notifications:newNotifications,
-          ...userTweet});
-        });
-        await getProfileDoc(targetUser).get().then(async(doc)=> {
-          const profile =doc.data();
-          const newNotifications= profile.notifications.filter(n=> n.user !== currentUser.uid);
-          await  getProfileDoc(targetUser).set({
-            notifications:newNotifications,
-            ...profile
+            
           });
-        });
-
-      });
+  
+          
+        })
+      };
+  
+      if(myProfile.follower[0]!== undefined){
+        myProfile.follower.forEach(async(user)=>{
+          await getProfileDoc(user).get().then(
+            (result) => {
+              const profile= result.data();
+              const newFollowing =profile.following.filter(f=> f!==userobj.uid);
+              getProfileDoc(user).set({
+                following: newFollowing
+              },{merge:true})
+            }
+          );
+        })
+      };
     };
-    // // clean up collection 
-    const deleteTweets = async()=>{ 
-      await getTweetsDocs(currentUser.uid).then(result=>{
-      const docs =result.docs;
-      docs.forEach(doc=> {
-        const id=doc.id;
-        dbService.collection(`tweets_${currentUser.uid}`).doc(id).delete();
+
+    const deleteTweets =async()=>{ 
+          //clean up rt,heart,answer,qt
+      // 유저가 작성한 tweet에 대한 rt.heart,qt 삭제
+      if(myProfile.notifications[0]!==undefined){
+        const targets =myProfile.notifications.filter(n=> n.docId !== null);
+        if(targets[0]!== undefined){
+          targets.forEach(async(target)=>{
+            getTweetsDocs(target.user, target.aboutDocId).then(
+              result=> {
+                const docs= result.docs;
+                docs.forEach(doc=> doc.delete())
+              }
+            );
+          })
+        };
+  
       }
-      )
+    // 유저가 rt.heart,qt,answer 한 것 에 대한 삭제
+    if(myTweets[0]!==undefined){
+      const targetTweets= myTweets.filter(tweet => tweet.about !== null);
+
+      if(targetTweets[0]!== undefined){
+        targetTweets.forEach(async(tweet)=>{
+          const targetUser = tweet.about.creatorId;
+          await getTweetDoc(targetUser, tweet.about.docId).then(doc=> {
+            const userTweet= doc.data();
+            const newNotifications= userTweet.notifications.filter(n=>n.user!== userobj.uid);
+            dbService.collection(`tweets_${targetUser}`).doc(`${tweet.about.docId}`).set({notifications:newNotifications,
+            ...userTweet});
+          });
+          await getProfileDoc(targetUser).get().then(async(doc)=> {
+            const profile =doc.data();
+            const newNotifications= profile.notifications.filter(n=> n.user !== userobj.uid);
+            await  getProfileDoc(targetUser).set({
+              notifications:newNotifications,
+              ...profile
+            });
+          });
+  
+        });
+      };
+    };
+
+      //collection 삭제
+      await getTweetsDocs(userobj.uid).then(result=>{
+      const docs =result.docs;
+      if(docs[0]!==undefined){
+        docs.forEach(doc=> {
+          const id=doc.id;
+          dbService.collection(`tweets_${userobj.uid}`).doc(id).delete();
+        }
+        )
+      }
     });
-    getProfileDoc(currentUser.uid).delete();
     }; 
 
-    deleteTweets();
+    const onDelete=()=>{
+      setLoading(true);
+      const storage= storageService.ref().child(`${userobj.uid}`);
+      const currentUser =authSerVice.currentUser;
+      currentUser.delete().then(()=>{
+        storage.delete();
+        cleanUpFollow();
+        deleteTweets();
+        getProfileDoc(userobj.uid).delete();
+        setDeleteError(false);
+        setLoading(false);
+        navigate('/twitter/');
+      }
+      ).catch(error=> {
+        console.log(error);
+      setDeleteError(true);
+      setLoading(false);});
+    };
 
-    const storage= storageService.ref().child(`${currentUser.uid}`);
-    storage.delete();
-
-    currentUser.delete().then(()=>{
-      setDeleteError(false)
+    const onConfirm =()=>{
+      
     }
-    ).catch(error=> {
-      console.log(error);
-    setDeleteError(true)});
+    return (
+      <section id="deleteUser">
+      {deleteError ?(
+      <div className='delete_inner' id="deleteError">
+        <div className="twitter_icon">
+        <BsTwitter/>
+        </div>
+        <div className='delete_alret'>
+          If you want to delete yout account,
+          <br/>
+          Please log out and try again.
+        </div>
+        <div className='deleteBtns' id="deleteErrorBtns">
+          <button onClick={()=>{navigate('/twitter/home')}}>
+            Cancel
+          </button>
+          <button onClick={()=>{navigate('/twitter/logOut')}}>
+            Log out
+          </button>
+        </div>
+      </div>)
+      :
+      (
+        <div  className='delete_inner'>
+          {
+            !loading?
+            <>
+              <div className="twitter_icon">
+                <BsTwitter/>
+              </div>
+              <div className='delete_alret'>
+                Do you want to delete your account?
+              </div>
+              <div className='deleteBtns' >
+                <button  id="deleteBtn" onClick={onDelete}>
+                  Delete
+                </button>
+              </div>
+            </>
+            : 
+            <Loading/>
+          }
+
+        </div>
+      )
+        }
+    </section>
+    )
   }
 
 
-export default React.memo(DeleteUser);
+export default DeleteUser;
