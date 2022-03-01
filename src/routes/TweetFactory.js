7fd2541937248149ec7f5f053932259e8890608f
BadaHertz52 <lmlwpeace@naver.com>
2022-03-01 17:26:20 +0900

    Update TweetFactory.js
    Cancel previous change

--- src/routes/TweetFactory.js
+++ src/routes/TweetFactory.js
@@ -158,7 +158,7 @@ const TweetFactory = ({userobj ,setPopup }) => {
   } = event ;
   const theFile =files[0];
   const reader = new FileReader();
-  reader.onload = (finishedEvent) =>{
+  reader.onloadend = (finishedEvent) =>{
     const { currentTarget : {result}} = finishedEvent;
     setAttachment (result);
     tweetDispatch({
