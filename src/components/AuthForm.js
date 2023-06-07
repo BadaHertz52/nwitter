79068daf78e99af97906d21cd50f8dfb74653598
badahertz52 <badahertz52@gmail.com>
2023-06-07 17:10:22 +0900

    [Updata] Add code that prevent XSS and  Make function that show error about email and pw

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,73 +1,113 @@
-import React, { useState } from 'react';
-import {BiArrowBack} from "react-icons/bi";
-import authSerVice from '../Fbase' ;
-import {RiErrorWarningLine} from 'react-icons/ri'
-import { useNavigate } from 'react-router-dom';
-import Footer from './Footer';
+import React, { useState } from "react";
+import { BiArrowBack } from "react-icons/bi";
+import authSerVice from "../Fbase";
+import { RiErrorWarningLine } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
 
-const AuthForm = ({newAcount, setPopup ,setOpenSusp})=> {
-  const [email ,setEmail ] = useState(""); 
-  const [password ,setPassword ] = useState("");
-  const [error, setError] = useState("") ;
-  const navigate =useNavigate();
+const AuthForm = ({ newAccount, setPopup, setOpenSusp }) => {
+  const [email, setEmail] = useState({ data: "", error: "required" });
+  const [password, setPassword] = useState({ data: "", error: "required" });
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
   const onChange = (event) => {
-    const {target :{name ,value}} =event ;
-    name === "email" ? setEmail(value) : setPassword(value) ;
+    const {
+      target: { name, value },
+    } = event;
+    const data = value.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+    const regExp = new RegExp(
+      "^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}$"
+    );
+    name === "email"
+      ? setEmail({
+          data: data,
+          error:
+            data === "" ? "required" : regExp.test(data) ? "pass" : "error",
+        })
+      : setPassword({ data: data, error: data === "" ? "required" : "pass" });
   };
-  const onSubmit =async(event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
-    try{
-      if(newAcount){
-        //create 
-          setOpenSusp(true);
-        //await authSerVice.createUserWithEmailAndPassword(email, password);
-        //navigate(`/twitter/home`) 
-      }else{
-        //log in 
-        await authSerVice.signInWithEmailAndPassword(email, password);
-        navigate(`/twitter/home`) 
+    try {
+      if (newAccount) {
+        //create
+        setOpenSusp(true);
+        // await authSerVice.createUserWithEmailAndPassword(
+        //   email.data,
+        //   password.data
+        // );
+        //navigate(`/twitter/home`)
+      } else {
+        //log in
+        await authSerVice.signInWithEmailAndPassword(email.data, password.data);
+        navigate(`/twitter/home`);
       }
       setPopup(false);
-      
-    }catch(e){
+    } catch (e) {
       const start = e.message.indexOf("Firebase:");
-      const end =e.message.indexOf("(")
-      setError(e.message.substring(start+10 ,end));
-    }
+      const end = e.message.indexOf("(");
 
-      
-    
-  } ;
+      console.log("e", e);
+      setError(e.message.substring(start + 10, end));
+    }
+  };
 
   return (
     <section id="authForm">
-      <button 
-      id="authForm_backBtn"
-      onClick={
-        ()=>{setPopup(false)}
-      }>
-        <BiArrowBack/>
+      <button
+        id="authForm_backBtn"
+        onClick={() => {
+          setPopup(false);
+        }}
+      >
+        <BiArrowBack />
       </button>
-      <form  onSubmit={onSubmit} >
-        <label for="email">Email </label>
-        <input id="email" name="email" type="text" placeholder="Email@***.***" onChange={onChange}   />
-        <label for="password">Password</label>
-        <input id="password" name="password" type="password"  placeholder="Password"  onChange={onChange} />
-        <input id="authForm_logIn" type="submit" value={newAcount ?"Sign up " : "Log In"} className='btn'/>
-        {error !=="" &&
+      <form onSubmit={onSubmit}>
+        <div className="form-email">
+          <label for="email">Email </label>
+          <input
+            id="email"
+            name="email"
+            type="text"
+            placeholder="Email@***.***"
+            onChange={onChange}
+          />
+          <div className="msg">
+            {email.error == "required" && "Please entere-mail"}
+            {email.error === "error" &&
+              "Please enter an email in the correct format"}
+          </div>
+        </div>
+        <div className="form-pw">
+          <label for="password">Password</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Password"
+            onChange={onChange}
+          />
+          <div className="msg">
+            {password.error === "required" && "Please enter password"}
+          </div>
+        </div>
+        <input
+          id="authForm_logIn"
+          type="submit"
+          disabled={!(email.error === "pass" && password.error === "pass")}
+          value={newAccount ? "Sign up " : "Log In"}
+          className="btn"
+        />
+        {error !== "" && (
           <div id="authError">
             <div>
-              <RiErrorWarningLine/>
+              <RiErrorWarningLine />
             </div>
-            <div>
-              {error}
-            </div>
+            <div className="authError__msg">{error}</div>
           </div>
-        }
-        
+        )}
       </form>
     </section>
-  )
-  } ;
+  );
+};
 
-export default React.memo(AuthForm) 
+export default React.memo(AuthForm);
